ad3cf0d6dd85c9afaae9207eaeb15bea878a420d
shenelle <shenellenono@gmail.com>
2026-07-01 06:19:55 +0000

    initial draft

--- ccprog3 exercises/Store.java
+++ ccprog3 exercises/Store.java
@@ -1,1 +1,45 @@
+public class Store {
+    public static int totalStoresOpened;
+    private String storeName;
+    private int stock = 0;
 
+    Store(String name, int totalStoresOpened)
+    {
+        storeName = name;
+        this.totalStoresOpened = totalStoresOpened + 1;
+    }
+
+    public void restock(int amount)
+    {
+        stock += amount;
+    }
+
+    public void sell(int amount)
+    {
+        stock -= amount;
+    }
+
+    public int getStock()
+    {
+        return stock;
+    }
+
+    static int getTotalStoresOpened()
+    {
+        return totalStoresOpened;
+    }
+
+    final void announce(String storeName)
+    {
+        System.out.println(storeName + "is open for business!");
+    }
+
+    public void compareStock(Store other)
+    {
+        if (other.stock > stock)
+            System.out.println(other.storeName + "has more stock.");
+
+        else
+            System.out.println(storeName + "has more stock.");
+    }
+}
